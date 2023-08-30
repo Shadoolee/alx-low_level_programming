5bf99db453d9f404f982ec0066fdf923aa436933
Shadoolee <oluwaseundvd@gmail.com>
2023-08-29 21:28:34 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop in a linked list using Floyd algorith
+ * @head: A pointer to head of the listint_t list.
+ * Return: The address of the node where the loop starts or NULL if no loop.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
